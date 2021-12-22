33613dacb3e5f597aca1fc4def43a98b6afa1155
fed <fcfinjap@yahoo.com>
2021-12-22 13:29:40 -0500

    decouple django secret key

--- rentals/settings.py
+++ rentals/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+
+SECRET_KEY = config("SECRET_KEY")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -18,9 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0l5hkly5+a_+$$f78q%%*9@-hp-x%2_20yn+u06jj#f8o$bs50'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
